fd1539be76a2eafd04468b5b3d75eb8c0985803c
Preveen Chandran <preveenchandran@gmail.com>
2017-08-21 11:24:27 +0530

    Added Contract Project

---
+++ HCContract/Patient/IPatientRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HCContract.Patient
+{
+    public interface IPatientRepository
+    {
+        ICollection<Patient> GetAllPatient();
+    }
+}
